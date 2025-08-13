ffb08eae72f24f47b7fc4058c8ba712eff8e2efe
JoseLovon <JLOVONVEGA01@mynbcc.ca>
2025-08-13 12:22:00 -0300

    completed

---
+++ frontend/src/api.ts
@@ -0,0 +1,85 @@
+import { Recipe } from "./types";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const searchRecipes = async (searchTerm: string, page: number) => {
+    console.log(`BASE_URL: ${API_BASE_URL}`);
+    const baseUrl = new URL(`${API_BASE_URL}/api/recipes/search`);
+    baseUrl.searchParams.append("searchTerm",searchTerm);
+    baseUrl.searchParams.append("page", String(page));
+
+    const response = await fetch(baseUrl);
+    if(!response.ok){
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+
+    return response.json();
+};
+
+const getRecipeSummary = async (recipeId: string) => {
+    const url = new URL(`${API_BASE_URL}/api/recipes/${recipeId}/summary`);
+    const response = await fetch(url);
+    
+    if(!response.ok){
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+
+    return response.json();
+};
+
+const getFavouriteRecipes = async () => {
+    const url = new URL(`${API_BASE_URL}/api/recipes/favourite`);
+    const response = await fetch(url);
+    
+    if(!response.ok){
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+
+    return response.json();
+};
+
+const addFavouriteRecipe = async(recipe: Recipe) => {
+    const url = new URL(`${API_BASE_URL}/api/recipes/favourite`);
+    const body = {
+        recipeId: recipe.id
+    }
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+
+    if(!response.ok){
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+};
+
+const removeFavouriteRecipe = async (recipe: Recipe) => {
+    const url = new URL(`${API_BASE_URL}/api/recipes/favourite`);
+    const body = {
+        recipeId: recipe.id
+    }
+
+    const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+
+    if(!response.ok){
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+};
+
+export {
+    searchRecipes,
+    getRecipeSummary,
+    getFavouriteRecipes,
+    addFavouriteRecipe,
+    removeFavouriteRecipe,
+};
